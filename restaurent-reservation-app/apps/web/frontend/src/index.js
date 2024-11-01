9d1914ec4bbc7a655d614afe469f16de77e4d21a
Abdul Moiz <abdul.moiz4677@gmail.com>
2024-11-01 22:57:47 +0500

    Resolved merge conflicts in package.json

--- restaurent-reservation-app/apps/web/frontend/src/index.js
+++ restaurent-reservation-app/apps/web/frontend/src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import './index.css';
-=======
 import './index.css'; // Include the CSS file for styling
->>>>>>> 4cf78863afb769952ad8869ea0205402e0c3918e
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -15,11 +11,5 @@ root.render(
   </React.StrictMode>
 );
 
-<<<<<<< HEAD
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-=======
 // Performance measurement setup
->>>>>>> 4cf78863afb769952ad8869ea0205402e0c3918e
 reportWebVitals();
