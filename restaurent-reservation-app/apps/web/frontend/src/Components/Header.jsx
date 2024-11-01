75d1552ff809e3c7200aa8b835ebfccfb11defa1
Saif3256 <167188461+Saif3256@users.noreply.github.com>
2024-11-01 19:41:27 +0500

    Header component make

---
+++ restaurent-reservation-app/apps/web/frontend/src/Components/Header.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+ 
+
+function Header() {
+  return (
+    <header>
+      {/* Desktop Header */}
+      <div className="container header my-3 d-none d-lg-flex">
+        <div className="logo">
+          <a href="./index.html">
+            <i className="fa fa-utensils me-3"></i>
+            <h1 className="mb-0">Restarent managment system</h1>
+          </a>
+        </div>
+        <div className="menus">
+          <ul className="d-flex mb-0">
+            <li className="list-unstyled py-2">
+              <a className="text-decoration-none text-uppercase p-4" href="./index.html">Home</a>
+            </li>
+            <li className="list-unstyled py-2">
+              <a className="text-decoration-none text-uppercase p-4" href="./about.html">About</a>
+            </li>
+            <li className="list-unstyled py-2">
+              <a className="text-decoration-none text-uppercase p-4" href="./reservation.html">Reservation</a>
+            </li>
+            <li className="list-unstyled py-2">
+              <a className="text-decoration-none text-uppercase p-4" href="./menu.html">Menu</a>
+            </li>
+            <li className="list-unstyled py-2">
+              <a className="text-decoration-none text-uppercase p-4" href="./contact.html">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="icons">
+          <a className="text-decoration-none" id="searchBtn" href="#">
+            <i className="fa fa-search me-3"></i>
+          </a>
+          <a className="text-decoration-none" id="shoppingbutton" href="#">
+            <i className="fa fa-shopping-bag me-3"></i>
+          </a>
+        </div>
+      </div>
+
+      {/* Mobile Header */}
+      <div className="d-flex justify-content-around py-3 align-items-center d-lg-none">
+        <div id="hamburger">
+          <i className="fa fa-2x fa-bars me-3 text-white"></i>
+        </div>
+        <div className="mobile-nav-logo">
+          <div className="logo">
+            <a href="./index.html">
+              <i className="fa fa-utensils me-3"></i>
+              <h1 className="mb-0">Restoran</h1>
+            </a>
+          </div>
+        </div>
+        <div className="mobile-nav-icons">
+          <div className="icons">
+            <a className="text-decoration-none" id="searchBtnMobile" href="#">
+              <i className="fa fa-search me-3"></i>
+            </a>
+            <a className="text-decoration-none" id="shoppingbuttonMobile" href="#">
+              <i className="fa fa-shopping-bag me-3"></i>
+            </a>
+          </div>
+        </div>
+        <div className="position-fixed w-75 bg-white h-100 top-0 start-0" id="mobile-menu">
+          <div id="hamburger-cross" className="d-flex justify-content-end align-items-center py-2">
+            <i className="fa fa-2x fa-plus me-3"></i>
+          </div>
+          <div className="menus">
+            <ul className="d-flex flex-column ps-2 mb-0 mt-4">
+              <li className="list-unstyled py-2">
+                <a className="text-dark text-decoration-none text-uppercase p-4" href="./index.html">Home</a>
+              </li>
+              <li className="list-unstyled py-2">
+                <a className="text-dark text-decoration-none text-uppercase p-4" href="./about.html">About</a>
+              </li>
+              <li className="list-unstyled py-2">
+                <a className="text-dark text-decoration-none text-uppercase p-4" href="./reservation.html">Reservation</a>
+              </li>
+              <li className="list-unstyled py-2">
+                <a className="text-dark text-decoration-none text-uppercase p-4" href="./menu.html">Menu</a>
+              </li>
+              <li className="list-unstyled py-2">
+                <a className="text-dark text-decoration-none text-uppercase p-4" href="./contact.html">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
