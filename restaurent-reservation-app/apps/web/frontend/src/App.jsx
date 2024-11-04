8386d43469558bc204c4230630c308b0b69cc0bb
Abdul Moiz <abdul.moiz4677@gmail.com>
2024-11-04 12:22:04 +0500

    css and image bug fixes

--- restaurent-reservation-app/apps/web/frontend/src/App.jsx
+++ restaurent-reservation-app/apps/web/frontend/src/App.jsx
@@ -3,7 +3,6 @@ import Header from "./Components/Header";
 import SearchBar from "./Components/Search_bar";
 import ShoppingCart from "./Components/shopping_cart";
 import Banner from "./Components/Banner";
-import Services from "./Components/Services";
 import MenuSection from "./Components/Our_Menu";
 import Testimonials from "./Components/Testimonials";
 import OurChefs from "./Components/Chefs";
